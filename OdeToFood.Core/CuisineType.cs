1bd94107ddbe748af54e0df4a33a9dbdd2758a18
Jing Fang <fajin@microsoft.com>
2019-05-09 22:39:56 +0800

    adding data access

---
+++ OdeToFood.Core/CuisineType.cs
@@ -0,0 +1,11 @@
+﻿namespace OdeToFood.Core
+{
+    public enum CuisineType
+    {
+        None,
+        Chinese,
+        Thai,
+        Japanese,
+        Korean
+    }
+}
---
+++ OdeToFood.Data/IRestaurantData.cs
@@ -0,0 +1,34 @@
+﻿using OdeToFood.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace OdeToFood.Data
+{
+    interface IRestaurantData
+    {
+        IEnumerable<Restaurant> GetAll();
+    }
+
+    public class InMemoryRestaurantData : IRestaurantData
+    {
+        readonly List<Restaurant> restaurants;
+
+        public InMemoryRestaurantData()
+        {
+            restaurants = new List<Restaurant>()
+            {
+                new Restaurant { Id = 1, Name = "May Flower", Cuisine = CuisineType.Chinese, Location = "New York"},
+                new Restaurant { Id = 2, Name = "Kong", Cuisine = CuisineType.Japanese, Location = "San Jose"},
+                new Restaurant { Id = 3, Name = "Jang Sujang", Cuisine = CuisineType.Korean, Location = "Seattle"}
+            };
+        }
+        public IEnumerable<Restaurant> GetAll()
+        {
+            return from r in restaurants
+                   orderby r.Id
+                   select r;
+        }
+    }
+}
